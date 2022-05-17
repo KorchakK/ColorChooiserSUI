842ae537f6266246d8a5fc5e5e1e198ca965e6f2
Korchak Konstantin <korchak.konstantin@gmail.com>
2022-05-17 17:53:54 +0300

    Initial Commit

---
+++ ColorChooiserSUI/ColorChooiserSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChooiserSUIApp.swift
+//  ColorChooiserSUI
+//
+//  Created by Konstantin Korchak on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorChooiserSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
